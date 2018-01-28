db3a73134aef68000eaa37872226ba61bc1787c3
radamosch <35626984+radamosch@users.noreply.github.com>
2018-01-28 18:07:07 +0000

    Update hedgely.sol

--- hedgely.sol
+++ hedgely.sol
@@ -1,19 +1,196 @@
 pragma solidity ^0.4.19;
 
-import "https://github.com/axiomzen/eth-random/contracts/Random.sol";
-
-
 // radamosch@gmail.com
 
 // Contract based investment game
-// 0x7d630aaa33fde0b099fa6b5c8c33e5aaaecd7b6c
+
+// 0xf8c4dbdc95c6bb06df29a15506f6186272c0894e
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+
+  address public owner;
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
 
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
 
-contract Hedgely {
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
 
 
-  address owner;
+/**
+ * @title Pausable
+ * @dev Base contract which allows children to implement an emergency stop mechanism.
+ */
+contract Pausable is Ownable {
+  event Pause();
+  event Unpause();
+
+  bool public paused = false;
+
+
+  /**
+   * @dev Modifier to make a function callable only when the contract is not paused.
+   */
+  modifier whenNotPaused() {
+    require(!paused);
+    _;
+  }
+
+  /**
+   * @dev Modifier to make a function callable only when the contract is paused.
+   */
+  modifier whenPaused() {
+    require(paused);
+    _;
+  }
+
+  /**
+   * @dev called by the owner to pause, triggers stopped state
+   */
+  function pause() onlyOwner whenNotPaused public {
+    paused = true;
+    Pause();
+  }
+
+  /**
+   * @dev called by the owner to unpause, returns to normal state
+   */
+  function unpause() onlyOwner whenPaused public {
+    paused = false;
+    Unpause();
+  }
+}
+
+/**
+ * @title Syndicate
+ * @dev Syndicated profit sharing - for early adopters
+ * Shares are not transferrable -
+ */
+contract Syndicate is Ownable,Pausable{
+
+    uint256 public totalSyndicateShares = 10000;
+    uint256 public availableSyndicateShares = 5000; // how many slots are available
+    //uint256 public maxSharesPerMember = 200; // !!??? todo
+    uint256 public syndicateSharePrice = 1 finney; // 0.001 ether
+    uint256 public shareCycleSessionSize = 10; // number of sessions in a share cycle
+    uint256 public shareCycleIndex = 0; // current position in share cycle
+    uint256 public currentSyndicateValue = 0; // total value of syndicate to be divided among memmbers
+    uint256 public numberSyndicateMembers = 0;
+    bool public shareCycleOverdue = false; // whether or not a profit sharing cycle should be processed.
+
+    struct member {
+        uint256 numShares;
+        uint256 profitShare;
+     }
+
+    address[] private syndicateMembers;
+    mapping(address => member ) private members;
+
+
+    event ProfitShare(
+          uint256 _currentSyndicateValue,
+          uint256 _numberSyndicateMembers,
+          uint256 _totalOwnedShares,
+          uint256 _profitPerShare
+    );
+
+    function Syndicate() public {
+        members[msg.sender].numShares = 5000; // owner portion
+        members[msg.sender].profitShare = 0;
+        numberSyndicateMembers = 1;
+        syndicateMembers.push(msg.sender);
+    }
+
+    // initiates a dividend oif necessary, sends
+    function claimDividend() public {
+      if (members[msg.sender].numShares==0) revert(); // only syndicate members.
+
+      // divide up the profits between the syndicate members
+      if (shareCycleOverdue){
+
+          uint256 totalOwnedShares = totalSyndicateShares-availableSyndicateShares;
+          uint256 profitPerShare = SafeMath.div(currentSyndicateValue,totalOwnedShares);
+
+          // foreach member , calculate their profitshare
+          for(uint i = 0; i< numberSyndicateMembers; i++)
+          {
+            // do += so that acrues across share cycles.
+            members[syndicateMembers[i]].profitShare+=SafeMath.mul(members[syndicateMembers[i]].numShares,profitPerShare);
+          }
+
+          // emit a profit share event
+          ProfitShare(currentSyndicateValue, numberSyndicateMembers, totalOwnedShares , profitPerShare);
+
+          currentSyndicateValue=0; // all the profit has been divided up
+          shareCycleOverdue = false;
+          shareCycleIndex = 0; // restart the share cycle count.
+      }
+      uint256 profitShare = members[msg.sender].profitShare;
+      if (profitShare>0){
+        members[msg.sender].profitShare = 0;
+        msg.sender.transfer(profitShare);
+      }
+    }
+
+    // allocate syndicate shares up to the limit.
+    function allocateShares(uint256 value) internal {
+         if (availableSyndicateShares==0) return;
+         uint256 allocation = SafeMath.div(value,syndicateSharePrice);
+         if (allocation >= availableSyndicateShares){
+            allocation = availableSyndicateShares; // limit hit
+         }
+         availableSyndicateShares = SafeMath.sub(availableSyndicateShares, allocation);
+
+         if (members[msg.sender].numShares == 0){
+          syndicateMembers.push(msg.sender);
+          numberSyndicateMembers++;
+         } // add new member of syndicate
+         members[msg.sender].numShares+=allocation;
+
+    }
+
+    // how many shares?
+    function memberShareCount() public  view returns (uint256) {
+        return members[msg.sender].numShares;
+    }
+
+}
+
+
+/**
+ * Core Hedgely Contract
+ */
+contract Hedgely is Ownable,Pausable, Syndicate {
 
    // Array of players
    address[] private players;
@@ -30,6 +207,9 @@ contract Hedgely {
 
    // The total amount of Ether bet for this current market
    uint256 public totalInvested;
+   // The amount of Ether used to see the market
+   uint256 public seedInvestment;
+
 
    // The total number of investments the users have made
    uint256 public numberOfInvestments;
@@ -73,11 +253,6 @@ contract Hedgely {
            uint256 _blockNumber
      );
 
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-
     bool locked;
     modifier noReentrancy() {
         require(!locked);
@@ -113,24 +288,28 @@ contract Hedgely {
         return numPlayers;
     }
 
-
-    Random api = Random(0x1637140C895e01d14be5a7A42Ec2c5BB22893713);
-
+    // generate a random number between 1 and 20 to seed a symbol
     function rand() internal returns (uint64) {
-      return api.random(19)+1;
+      return random(19)+1;
     }
+
+    // pseudo random - but does that matter?
+    uint64 _seed = 0;
+    function random(uint64 upper) private returns (uint64 randomNumber) {
+       _seed = uint64(keccak256(keccak256(block.blockhash(block.number), _seed), now));
+       return _seed % upper;
+     }
 
     // resets the market conditions
    function resetMarket() internal {
 
       sessionNumber ++;
-      totalInvested = 0;
       startingBlock = block.number;
       endingBlock = startingBlock + sessionBlockSize; // approximately every 5 minutes - can play with this
       clearPlayers();
 
      uint256 sumInvested = 0;
-     
+
      for(uint i=0;i<10;i++)
       {
           uint256 num =  rand();
@@ -139,7 +318,8 @@ contract Hedgely {
       }
 
      playerPortfolio[this] = marketOptions;
-     totalInvested = totalInvested + sumInvested;
+     totalInvested =  sumInvested;
+     seedInvestment = sumInvested;
      insertPlayer(this);
      numPlayers=1;
      numberOfInvestments = 10;
@@ -147,8 +327,7 @@ contract Hedgely {
      currentLowest = findCurrentLowest();
      StartSession(sessionNumber, sessionBlockSize, marketOptions , startingBlock);
 
-
-   } 
+   }
 
 
     function roundIt(uint256 amount) internal constant returns (uint256)
@@ -159,7 +338,7 @@ contract Hedgely {
     }
 
     // main entry point for investors/players
-    function invest(uint256 optionNumber) public payable noReentrancy {
+    function invest(uint256 optionNumber) public payable noReentrancy whenNotPaused {
 
       // Check that the number to bet is within the range
       assert(optionNumber >= 0 && optionNumber <= 9);
@@ -179,10 +358,13 @@ contract Hedgely {
                     insertPlayer(msg.sender);
                     activePlayers[msg.sender]=true;
        }
-     
+
       Invest(msg.sender, optionNumber, amount, marketOptions, block.number);
 
-       currentLowest = findCurrentLowest();
+      // possibly allocate syndicate shares
+      allocateShares(amount);
+
+      currentLowest = findCurrentLowest();
       if (block.number >= endingBlock && currentLowestCount==1) distributeWinnings();
 
     } // end invest
@@ -221,12 +403,14 @@ contract Hedgely {
         // record the end of session
         EndSession(sessionNumber, numberWinner, marketOptions , block.number);
 
+        uint256 sessionWinnings = 0;
         for(uint j=1;j<numPlayers;j++)
         {
               if (playerPortfolio[players[j]][numberWinner]>0){
                 uint256 winningAmount =  playerPortfolio[players[j]][numberWinner];
                 uint256 winnings = SafeMath.mul(8,winningAmount); // eight times the invested amount.
                 totalHedgelyWinnings+=winnings;
+                sessionWinnings+=winnings;
                 players[j].transfer(winnings); // don't throw here
               }
               playerPortfolio[players[j]] = [0,0,0,0,0,0,0,0,0,0];
@@ -234,7 +418,18 @@ contract Hedgely {
 
         }
 
+        // calculate session profit and add to the syndicated value
+        uint256 sessionProfit = SafeMath.sub(SafeMath.sub(totalInvested,seedInvestment),sessionWinnings);
+        currentSyndicateValue+=sessionProfit;
+
         resetMarket();
+
+        // increment share cycle index as this session has ended
+        shareCycleIndex+=1;
+        if (!shareCycleOverdue && shareCycleIndex >= shareCycleSessionSize){
+          shareCycleOverdue = true; // allows syndicate members to claim dividend
+        }
+
     }
 
 
@@ -259,11 +454,9 @@ contract Hedgely {
    // Could be needed
     function withdraw(uint256 amount) public onlyOwner {
         require(amount<=this.balance);
-
         if (amount==0){
             amount=this.balance;
         }
-
         owner.transfer(amount);
     }
 
